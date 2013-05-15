2a519d532b851690f8bd9de97b0c730776bea61e
Aurindam Jana <aurindam.jana@digia.com>
2013-05-15 17:08:35 +0200

    Jolla: Yaml Updater refactor code

--- src/plugins/mer/meryamlupdater.cpp
+++ src/plugins/mer/meryamlupdater.cpp
@@ -52,27 +52,16 @@ MerYamlUpdater::MerYamlUpdater(QObject *parent) :
 
 void MerYamlUpdater::init()
 {
+    // This is called just once after the MerSdkManager is initialized.
     const SessionManager *sessionManager = ProjectExplorerPlugin::instance()->session();
-
-    const QList<Kit *> merKits = MerSdkManager::instance()->merKits();
     const QList<Project *> projects = sessionManager->projects();
     // O(n2)
-    foreach (const Project *project, projects) {
+    foreach (Project *project, projects) {
         connect(project, SIGNAL(addedTarget(ProjectExplorer::Target*)),
                 SLOT(onTargetAddedToProject(ProjectExplorer::Target*)));
         connect(project, SIGNAL(removedTarget(ProjectExplorer::Target*)),
                 SLOT(onTargetRemovedFromProject(ProjectExplorer::Target*)));
-        foreach (Kit *kit, merKits) {
-            if (project->supportsKit(kit)) {
-                const Qt4Project *qt4Project = qobject_cast<const Qt4Project *>(project);
-                if (!qt4Project)
-                    continue;
-                m_projectsToMonitor << project;
-                connect(qt4Project, SIGNAL(proFilesEvaluated()), SLOT(onProFilesEvaluated()));
-                onProFilesEvaluated(project);
-                break;
-            }
-        }
+        onProjectAdded(project);
     }
 
     connect(sessionManager, SIGNAL(projectAdded(ProjectExplorer::Project*)),
@@ -83,7 +72,6 @@ void MerYamlUpdater::init()
 
 void MerYamlUpdater::onProFilesEvaluated(const Project *proj)
 {
-
     const Qt4Project *project = proj ? qobject_cast<const Qt4Project *>(proj)
                                      : qobject_cast<const Qt4Project *>(sender());
     if (!project || !m_projectsToMonitor.contains(project))
@@ -107,38 +95,54 @@ void MerYamlUpdater::onProFilesEvaluated(const Project *proj)
             break;
         }
     }
+    // Find the yaml in /rpm folder
+    if (yamlFile.isEmpty()) {
+        QDir projDir(proj->projectDirectory());
+        QStringList rpm = projDir.entryList(QStringList() << QLatin1String("rpm"), QDir::Dirs);
+        if (rpm.count()) {
+            QDir rpmDir(rpm.first());
+            QStringList files = rpmDir.entryList(QStringList() << QString::fromLatin1("%1.yaml").arg(project->displayName()), QDir::Files);
+            if (files.count())
+                yamlFile = files.first();
+        }
+    }
     QByteArray newContent;
     {
         Utils::FileReader yamlReader;
         if (!yamlReader.fetch(yamlFile, QIODevice::ReadOnly))
             return;
         const QByteArray data = yamlReader.data();
-        // "Files:(.|(\\s+-\\s+)|\\n)*(?:%1)(.|(\\s+-\\s+)|\\n)*(?:%2)"
         QRegExp rxp(QString::fromLatin1("(Files:.*)(?:%1\n)(.*)(?:%2)(.*:?)").arg(QLatin1String(SECTION_START), QLatin1String(SECTION_END)));
         rxp.setMinimal(true);
         int pos = 0;
         if ((pos = rxp.indexIn(QString::fromUtf8(data), pos)) != -1) {
+            // Add everything before the Files: section
             newContent = data.left(pos);
+
+            // Add everything before the comments
             newContent.append(rxp.cap(1).toUtf8());
 
-            newContent.append(SECTION_START);
-            newContent.append("\n");
-            QString prefix;
+            // Find out the whitespace used in this section
+            QString prefix = QLatin1String("  - ");
             const QRegExp rxPrefix(QLatin1String("\n+(\\s*-\\s*)"));
             if (rxPrefix.indexIn(rxp.cap(1)) != -1)
                 prefix = rxPrefix.cap(1);
             else if (rxPrefix.indexIn(rxp.cap(3)) != -1)
                 prefix = rxPrefix.cap(1);
-            else
-                prefix = QLatin1String("  - ");
+
+            // Overwrite the contents between the comments
+            newContent.append(SECTION_START);
+            newContent.append("\n");
             foreach (const QString &target, targetPaths)
                 newContent.append(prefix.toUtf8() + '"' + target.toUtf8() + "\"\n");
             newContent.append(SECTION_END);
+
+            // Add everything after the comments
             pos += rxp.matchedLength();
             newContent.append(data.mid(pos));
         }
     }
-    {
+    if (!newContent.isEmpty()) {
         Utils::FileSaver yamlSaver(yamlFile, QIODevice::WriteOnly);
         yamlSaver.write(newContent);
         yamlSaver.finalize();
@@ -150,12 +154,7 @@ void MerYamlUpdater::onTargetAddedToProject(Target *target)
     const Project *project = target->project();
     if (MerSdkManager::instance()->merKits().contains(target->kit())
             && !m_projectsToMonitor.contains(project)) {
-        const Qt4Project *qt4Project = qobject_cast<const Qt4Project *>(project);
-        if (qt4Project) {
-            m_projectsToMonitor.append(project);
-            connect(qt4Project, SIGNAL(proFilesEvaluated()), SLOT(onProFilesEvaluated()));
-            onProFilesEvaluated(project);
-        }
+        monitorProject(project);
     }
 }
 
@@ -182,13 +181,8 @@ void MerYamlUpdater::onProjectAdded(Project *project)
     const QList<Kit *> merKits = MerSdkManager::instance()->merKits();
     foreach (Kit *kit, merKits) {
         if (project->supportsKit(kit)) {
-            const Qt4Project *qt4Project = qobject_cast<const Qt4Project *>(project);
-            if (!qt4Project)
-                continue;
-            m_projectsToMonitor << project;
-            connect(qt4Project, SIGNAL(proFilesEvaluated()), SLOT(onProFilesEvaluated()));
-            onProFilesEvaluated(project);
-            break;
+            if (monitorProject(project))
+                break;
         }
     }
 }
@@ -197,6 +191,17 @@ void MerYamlUpdater::onProjectRemoved(Project *project)
 {
     m_projectsToMonitor.removeOne(project);
     project->disconnect(this);
+}
+
+bool MerYamlUpdater::monitorProject(const Project *project)
+{
+    const Qt4Project *qt4Project = qobject_cast<const Qt4Project *>(project);
+    if (!qt4Project)
+        return false;
+    m_projectsToMonitor.append(project);
+    connect(qt4Project, SIGNAL(proFilesEvaluated()), SLOT(onProFilesEvaluated()));
+    onProFilesEvaluated(project);
+    return true;
 }
 
 } // Internal
