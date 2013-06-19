d3f1f38be31452bcae61ec86d022b774e39a758d
Michal Klocek <michal.klocek@digia.com>
2013-06-19 10:05:46 +0200

    RemoteLinux: Allow to override defaultRemoteExecutableFilePath()
    (extracted from commit e6221ed79a5adefd0fa46a32106e11770a4974a0)

--- src/plugins/remotelinux/remotelinuxrunconfiguration.h
+++ src/plugins/remotelinux/remotelinuxrunconfiguration.h
@@ -65,7 +65,7 @@ public:
     ProjectExplorer::Runnable runnable() const override;
 
     QString localExecutableFilePath() const;
-    QString defaultRemoteExecutableFilePath() const;
+    virtual QString defaultRemoteExecutableFilePath() const;
     QString remoteExecutableFilePath() const;
     QString arguments() const;
     void setArguments(const QString &args);
