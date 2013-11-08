0760c125897c6bc0c18166281cc16bc56f4e7e76
Petref Saraci <petref.saraci@digia.com>
2013-11-08 17:25:54 +0100

    Handle emulator installation/deinstallation as per sdk settings
    (adapted from commit 8bf6059f6fa70719057daac7ae398bf1534d63e2)

--- src/plugins/projectexplorer/devicesupport/idevice.h
+++ src/plugins/projectexplorer/devicesupport/idevice.h
@@ -138,6 +138,7 @@ public:
 
     Core::Id type() const;
     bool isAutoDetected() const;
+    bool isSdkProvided() const;
     Core::Id id() const;
 
     virtual bool isCompatibleWith(const Kit *k) const;
@@ -206,6 +207,7 @@ private:
     IDevice &operator=(const IDevice &); // Unimplemented.
 
     int version() const;
+    void setSdkProvided(bool sdkProvided);
 
     Internal::IDevicePrivate *d;
     friend class DeviceManager;
