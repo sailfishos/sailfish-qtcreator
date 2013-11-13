1a4b10ebb1408512e49dccac111a6bde77534d92
Petref Saraci <petref.saraci@digia.com>
2013-11-13 18:27:34 +0100

    Potential fix on device manager

--- src/plugins/projectexplorer/devicesupport/devicemanager.cpp
+++ src/plugins/projectexplorer/devicesupport/devicemanager.cpp
@@ -201,6 +201,7 @@ void DeviceManager::load()
             if (device->id() == sdkDevice->id()) {
                 if (device->version() < sdkDevice->version())
                     device = sdkDevice;
+                addDevice(device);
                 sdkDevices.removeOne(sdkDevice);
                 break;
             }
