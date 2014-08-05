d08b61e1df6a2f9eacd02d3d31e38e9b6c50d866
Jarkko Lehtoranta <jarkko.lehtoranta@jolla.com>
2014-08-05 13:56:35 +0300

    sdktool: Allow sdktool to write its name into the xml headers
    Cherry-picked from 85a895f.
    
    Signed-off-by: Jarkko Lehtoranta <jarkko.lehtoranta@jolla.com>
    (cherry picked from commit 5dfb62309f7102e152898795bc35e0a3364dcb0a)

--- src/tools/sdktool/main.cpp
+++ src/tools/sdktool/main.cpp
@@ -45,6 +45,8 @@
 
 #include <iostream>
 
+#include <app/app_version.h>
+
 #include <QCoreApplication>
 #include <QStringList>
 
@@ -163,6 +165,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setSetuidAllowed(true);
 
     QCoreApplication a(argc, argv);
+
+    QCoreApplication::setApplicationName(QLatin1String("sdktool"));
+    QCoreApplication::setApplicationVersion(QLatin1String(Core::Constants::IDE_VERSION_LONG));
 
     Settings settings;
 
