2bf84015fbfbfcc93c8a8317bc1ebc6c3a84ebb0
Jarkko Lehtoranta <jarkko.lehtoranta@jolla.com>
2014-08-22 16:52:39 +0300

    Jolla: Add install_dir/lib to the rpaths
    This is needed for libicu, but also other additional 3rdparty libraries
    can now be included directly under install_dir/lib.
    
    Signed-off-by: Jarkko Lehtoranta <jarkko.lehtoranta@jolla.com>

--- scripts/deployqt.py
+++ scripts/deployqt.py
@@ -91,7 +91,8 @@ def fix_rpaths_helper(chrpath_bin, install_dir, dirpath, filenames):
     for filename in filenames:
         fpath = os.path.join(dirpath, filename)
         relpath = os.path.relpath(install_dir+'/lib/qtcreator', dirpath)
-        command = [chrpath_bin, '-r', '$ORIGIN/'+relpath, fpath]
+        relpath2 = os.path.relpath(install_dir+'/lib', dirpath)
+        command = [chrpath_bin, '-r', '$ORIGIN/'+relpath+':$ORIGIN/'+relpath2, fpath]
         print fpath, ':', command
         try:
             subprocess.check_call(command)
