175c0c563abbfc484fb9e10fabf5a1b9cbe06c06
Martin Kampas <martin.kampas@jolla.com>
2014-09-23 09:06:17 +0200

    Mer: Support more than one SDK VM in mgmt web view
    Signed-off-by: Martin Kampas <martin.kampas@jolla.com>

--- src/plugins/mer/mermanagementwebview.cpp
+++ src/plugins/mer/mermanagementwebview.cpp
@@ -22,24 +22,200 @@
 
 #include "mermanagementwebview.h"
 #include "ui_mermanagementwebview.h"
+#include "mersdkkitinformation.h"
 #include "mersdkmanager.h"
+
+#include <utils/qtcassert.h>
+
+#include <projectexplorer/project.h>
+#include <projectexplorer/session.h>
+#include <projectexplorer/target.h>
+
 #include <QTimer>
 
 namespace Mer {
 namespace Internal {
 
-const char CONTROLCENTER_URL[] = "http://127.0.0.1:8080/";
+const char CONTROLCENTER_URL_BASE[] = "http://127.0.0.1/";
+
+class MerManagementWebViewSdksModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    MerManagementWebViewSdksModel(QObject *parent);
+
+    MerSdk *sdkAt(int row) const;
+    int activeSdkIndex() const;
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role) const;
+
+signals:
+    void activeSdkIndexChanged(int index);
+
+private slots:
+    void onStartupProjectChanged(ProjectExplorer::Project *project);
+    void onActiveTargetChanged(ProjectExplorer::Target *target);
+    void onSdksUpdated();
+    void beginReset();
+    void endReset();
+
+private:
+    QList<MerSdk *> m_sdks;
+    QTimer *const m_endResetTimer;
+    ProjectExplorer::Project *m_startupProject;
+    MerSdk *m_activeSdk;
+};
+
+MerManagementWebViewSdksModel::MerManagementWebViewSdksModel(QObject *parent)
+    : QAbstractListModel(parent)
+    , m_endResetTimer(new QTimer(this))
+    , m_startupProject(0)
+    , m_activeSdk(0)
+{
+    m_endResetTimer->setSingleShot(true);
+    connect(m_endResetTimer, SIGNAL(timeout()), this, SLOT(endReset()));
+
+    connect(MerSdkManager::instance(), SIGNAL(initialized()), this, SLOT(onSdksUpdated()));
+    connect(MerSdkManager::instance(), SIGNAL(sdksUpdated()), this, SLOT(onSdksUpdated()));
+
+    connect(ProjectExplorer::SessionManager::instance(),
+            SIGNAL(startupProjectChanged(ProjectExplorer::Project*)),
+            this,
+            SLOT(onStartupProjectChanged(ProjectExplorer::Project*)));
+}
+
+MerSdk *MerManagementWebViewSdksModel::sdkAt(int row) const
+{
+    // generosity for easier use with respect to the "<no SDK>" row
+    if (m_sdks.count() == 0 && row == 0)
+        return 0;
+    if (row == -1)
+        return 0;
+    QTC_ASSERT(row >= 0 && row < m_sdks.count(), return 0);
+
+    return static_cast<MerSdk *>(m_sdks.at(row));
+}
+
+int MerManagementWebViewSdksModel::activeSdkIndex() const
+{
+    return m_sdks.indexOf(m_activeSdk);
+}
+
+int MerManagementWebViewSdksModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+    else if (m_sdks.isEmpty())
+        return 1;
+    else
+        return m_sdks.count();
+}
+
+QVariant MerManagementWebViewSdksModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    if (m_sdks.isEmpty()) {
+        switch (role) {
+        case Qt::DisplayRole:
+            return tr("<no SDK>");
+        case Qt::ToolTipRole:
+            return tr("Configure a Mer SDK in Options");
+        default:
+            return QVariant();
+        }
+    } else {
+        MerSdk *sdk = m_sdks.at(index.row());
+
+        switch (role) {
+        case Qt::DisplayRole:
+        case Qt::ToolTipRole:
+            return sdk->virtualMachineName();
+        default:
+            return QVariant();
+        }
+    }
+}
+
+void MerManagementWebViewSdksModel::onStartupProjectChanged(ProjectExplorer::Project *project)
+{
+    if (m_startupProject) {
+        disconnect(m_startupProject, SIGNAL(activeTargetChanged(ProjectExplorer::Target*)),
+                this, SLOT(onActiveTargetChanged(ProjectExplorer::Target*)));
+    }
+
+    m_startupProject = project;
+
+    if (m_startupProject) {
+        connect(m_startupProject, SIGNAL(activeTargetChanged(ProjectExplorer::Target*)),
+                this, SLOT(onActiveTargetChanged(ProjectExplorer::Target*)));
+        onActiveTargetChanged(m_startupProject->activeTarget());
+    } else {
+        onActiveTargetChanged(0);
+    }
+}
+
+void MerManagementWebViewSdksModel::onActiveTargetChanged(ProjectExplorer::Target *target)
+{
+    int oldActiveSdkIndex = activeSdkIndex();
+
+    if (target && MerSdkManager::isMerKit(target->kit())) {
+        m_activeSdk = MerSdkKitInformation::sdk(target->kit());
+        QTC_CHECK(m_activeSdk);
+    } else {
+        m_activeSdk = 0;
+    }
+
+    if (activeSdkIndex() != oldActiveSdkIndex) {
+        emit activeSdkIndexChanged(activeSdkIndex());
+    }
+}
+
+void MerManagementWebViewSdksModel::onSdksUpdated()
+{
+    QList<MerSdk *> oldSdks = m_sdks;
+
+    m_sdks = MerSdkManager::instance()->sdks();
+
+    if (m_sdks != oldSdks) {
+        beginReset();
+    }
+}
+
+void MerManagementWebViewSdksModel::beginReset()
+{
+    if (!m_endResetTimer->isActive()) {
+        beginResetModel();
+        m_endResetTimer->start();
+    }
+}
+
+void MerManagementWebViewSdksModel::endReset()
+{
+    endResetModel();
+}
+
+/*
+ * \class MerManagementWebView
+ */
 
 MerManagementWebView::MerManagementWebView(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::MerManagementWebView)
+    , m_sdksModel(new MerManagementWebViewSdksModel(this))
     , m_loaded(false)
     , m_autoFailReload(false)
 {
     ui->setupUi(this);
+    ui->sdksComboBox->setModel(m_sdksModel);
+    connect(m_sdksModel, SIGNAL(modelReset()), this, SLOT(selectActiveSdkVm()));
+    connect(m_sdksModel, SIGNAL(activeSdkIndexChanged(int)), this, SLOT(selectActiveSdkVm()));
+    selectActiveSdkVm();
     connect(ui->webView->page(), SIGNAL(linkHovered(QString,QString,QString)), ui->statusBarLabel, SLOT(setText(QString)));
     connect(ui->webView->page(), SIGNAL(loadFinished(bool)), SLOT(handleLoadFinished(bool)));
-    on_homeButton_clicked();
 }
 
 MerManagementWebView::~MerManagementWebView()
@@ -47,41 +223,69 @@ MerManagementWebView::~MerManagementWebView()
     delete ui;
 }
 
-void MerManagementWebView::setUrl(const QUrl &url)
+void MerManagementWebView::resetWebView()
 {
+    QUrl url;
+
+    if (m_selectedSdk) {
+        disconnect(m_selectedSdk->connection(), SIGNAL(virtualMachineOffChanged(bool)),
+                ui->webView, SLOT(reload()));
+    }
+
+    m_selectedSdk = m_sdksModel->sdkAt(ui->sdksComboBox->currentIndex());
+
+    if (m_selectedSdk) {
+        url = QUrl(QLatin1String(CONTROLCENTER_URL_BASE));
+        url.setPort(m_selectedSdk->wwwPort());
+        connect(m_selectedSdk->connection(), SIGNAL(virtualMachineOffChanged(bool)),
+                ui->webView, SLOT(reload()));
+    } else {
+        url = QLatin1String("about:blank");
+    }
+
     ui->webView->setUrl(url);
 }
 
 void MerManagementWebView::on_webView_urlChanged(const QUrl &url)
 {
-    ui->urlLineEdit->setText(url.toString());
+    Q_UNUSED(url);
     ui->statusBarLabel->clear();
 }
 
-void MerManagementWebView::on_urlLineEdit_returnPressed()
+void MerManagementWebView::on_sdksComboBox_currentIndexChanged(int index)
 {
-    setUrl(QUrl::fromUserInput(ui->urlLineEdit->text()));
+    Q_UNUSED(index);
+    resetWebView();
 }
 
 void MerManagementWebView::on_homeButton_clicked()
 {
-    setUrl(QUrl(QLatin1String(CONTROLCENTER_URL)));
+    resetWebView();
+}
+
+void MerManagementWebView::selectActiveSdkVm()
+{
+    ui->sdksComboBox->setCurrentIndex(qMax(0, m_sdksModel->activeSdkIndex()));
 }
 
 void MerManagementWebView::handleLoadFinished(bool success)
 {
-    if (!success && ui->webView->url().toString().indexOf(QLatin1String(CONTROLCENTER_URL)) != -1) {
+    if (!success) {
+        // one cannot be sure here
+        QString vmName = m_selectedSdk ? m_selectedSdk->virtualMachineName() : QLatin1String("SDK");
+
         ui->webView->setHtml(
-                             QLatin1String(
+                             QString::fromLatin1(
                                            "<html>"
                                            "<head></head>"
                                            "<body>"
                                            "<div style='text-align: center; vertical-align: middle;'>"
-                                           "<h1>The SDK VM is not responding.</h1>"
+                                           "<h1>The \"%1\" VM is not responding.</h1>"
                                            "<p><h1>Create a new SailfishOS project (or open an existing one) and press the <em>Start SDK</em> button on the lower left.</h1></p>"
                                            "</div>"
                                            "</body>"
                                            "</html>")
+                             .arg(vmName)
                              );
         m_loaded = false;
         if (m_autoFailReload)
@@ -94,7 +298,7 @@ void MerManagementWebView::handleLoadFinished(bool success)
 void MerManagementWebView::reloadPage()
 {
     if (ui->webView->url().isEmpty() || ui->webView->url().toString() == QLatin1String("about:blank"))
-        setUrl(QUrl(QLatin1String(CONTROLCENTER_URL)));
+        resetWebView();
 }
 
 void MerManagementWebView::setAutoFailReload(bool enabled)
@@ -106,3 +310,5 @@ void MerManagementWebView::setAutoFailReload(bool enabled)
 
 } // namespace Internal
 } // namespace Mer
+
+#include "mermanagementwebview.moc"
