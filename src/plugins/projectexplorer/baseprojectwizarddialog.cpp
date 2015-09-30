sailfishos/sailfish-qtcreator

commit e6cadea29b7588866e5b42ff78f31628cc2e4c76

Reintroduce "Jolla: Add preferredFeauters to example template" - non-Mer part
This reintroduces the non-Mer part of commit
af0ca3f9147312790ed27efd00d7c010edd09df3, temporarily reverted prior to
mer-3.1 merge with commit cea095600a2701d2e7d51b3d7d3a7c4612baf90a.

Signed-off-by: Martin Kampas <martin.kampas@jolla.com>

src/plugins/projectexplorer/baseprojectwizarddialog.cpp
@@ -54,6 +54,7 @@ struct BaseProjectWizardDialogPrivate {
     int introPageId;
     QString selectedPlatform;
     Core::FeatureSet requiredFeatureSet;
+    Core::FeatureSet preferredFeatureSet;
 };
 
 BaseProjectWizardDialogPrivate::BaseProjectWizardDialogPrivate(Utils::ProjectIntroPage *page, int id) :
@@ -70,6 +71,7 @@ BaseProjectWizardDialog::BaseProjectWizardDialog(QWidget *parent,
 {
     setPath(parameters.defaultPath());
     setSelectedPlatform(parameters.selectedPlatform());
+    setPrefferedFeatures(parameters.preferredFeatures());
     setRequiredFeatures(parameters.requiredFeatures());
     init();
 }
@@ -83,6 +85,7 @@ BaseProjectWizardDialog::BaseProjectWizardDialog(Utils::ProjectIntroPage *introP
 {
     setPath(parameters.defaultPath());
     setSelectedPlatform(parameters.selectedPlatform());
+    setPrefferedFeatures(parameters.preferredFeatures());
     setRequiredFeatures(parameters.requiredFeatures());
     init();
 }
@@ -206,6 +209,16 @@ Core::FeatureSet BaseProjectWizardDialog::requiredFeatures() const
 void BaseProjectWizardDialog::setRequiredFeatures(const Core::FeatureSet &featureSet)
 {
     d->requiredFeatureSet = featureSet;
+}
+
+Core::FeatureSet BaseProjectWizardDialog::preferredFeatures() const
+{
+    return d->preferredFeatureSet;
+}
+
+void BaseProjectWizardDialog::setPrefferedFeatures(const Core::FeatureSet &featureSet)
+{
+    d->preferredFeatureSet = featureSet;
 }
 
 } // namespace ProjectExplorer

src/plugins/projectexplorer/baseprojectwizarddialog.h
@@ -81,6 +81,9 @@ protected:
     Core::FeatureSet requiredFeatures() const;
     void setRequiredFeatures(const Core::FeatureSet &featureSet);
 
+    Core::FeatureSet preferredFeatures() const;
+    void setPrefferedFeatures(const Core::FeatureSet &featureSet);
+
 private slots:
     void slotAccepted();
     void nextClicked();

src/plugins/coreplugin/basefilewizard.cpp
@@ -218,6 +218,7 @@ void BaseFileWizard::runWizard(const QString &path, QWidget *parent, const QStri
                                                                                            allExtensionPages,
                                                                                            platform,
                                                                                            requiredFeatures(),
+                                                                                           preferredFeatures(),
                                                                                            dialogParameterFlags,
                                                                                            extraValues)));
     QTC_ASSERT(!wizard.isNull(), return);

src/plugins/coreplugin/basefilewizard.h
@@ -66,12 +66,13 @@ public:
 
     explicit WizardDialogParameters(const QString &defaultPath, const WizardPageList &extensionPages,
                                     const QString &platform, const Core::FeatureSet &requiredFeatures,
-                                    DialogParameterFlags flags,
+                                    const Core::FeatureSet &preferredFeatures, DialogParameterFlags flags,
                                     QVariantMap extraValues)
         : m_defaultPath(defaultPath),
           m_extensionPages(extensionPages),
           m_selectedPlatform(platform),
           m_requiredFeatures(requiredFeatures),
+          m_preferredFeatures(preferredFeatures),
           m_parameterFlags(flags),
           m_extraValues(extraValues)
     {}
@@ -88,6 +89,9 @@ public:
     Core::FeatureSet requiredFeatures() const
     { return m_requiredFeatures; }
 
+    Core::FeatureSet preferredFeatures() const
+    { return m_preferredFeatures; }
+
     DialogParameterFlags flags() const
     { return m_parameterFlags; }
 
@@ -99,6 +103,7 @@ private:
     WizardPageList m_extensionPages;
     QString m_selectedPlatform;
     Core::FeatureSet m_requiredFeatures;
+    Core::FeatureSet m_preferredFeatures;
     DialogParameterFlags m_parameterFlags;
     QVariantMap m_extraValues;
 };

src/plugins/coreplugin/dialogs/iwizard.h
@@ -70,6 +70,7 @@ public:
         QString category;
         QString displayCategory;
         FeatureSet requiredFeatures;
+        FeatureSet preferredFeatures;
         IWizard::WizardFlags flags;
         QString descriptionImage;
     };
@@ -85,6 +86,7 @@ public:
     QString displayCategory() const { return m_data.displayCategory; }
     QString descriptionImage() const { return m_data.descriptionImage; }
     FeatureSet requiredFeatures() const { return m_data.requiredFeatures; }
+    FeatureSet preferredFeatures() const { return m_data.preferredFeatures; }
     WizardFlags flags() const { return m_data.flags; }
 
     void setData(const Data &data) { m_data = data; }
@@ -98,6 +100,8 @@ public:
     void setDescriptionImage(const QString &descriptionImage) { m_data.descriptionImage = descriptionImage; }
     void setRequiredFeatures(const FeatureSet &featureSet) { m_data.requiredFeatures = featureSet; }
     void addRequiredFeature(const Feature &feature) { m_data.requiredFeatures |= feature; }
+    void setPreferredFeatures(const FeatureSet &featureSet) { m_data.preferredFeatures = featureSet; }
+    void addPreferredFeature(const Feature &feature) { m_data.preferredFeatures |= feature; }
     void setFlags(WizardFlags flags) { m_data.flags = flags; }
 
     virtual void runWizard(const QString &path, QWidget *parent, const QString &platform, const QVariantMap &variables) = 0;

src/plugins/coreplugin/featureprovider.h
@@ -73,6 +73,8 @@ public:
 
     FeatureSet(const FeatureSet &other) : QSet<Feature>(other) {}
 
+    using QSet<Feature>::isEmpty;
+
     FeatureSet &operator=(const FeatureSet &other)
     {
        QSet<Feature>::operator=(other);

src/plugins/projectexplorer/customwizard/customwizardparameters.cpp
@@ -67,6 +67,7 @@ static const char langAttributeC[] = "xml:lang";
 static const char categoryAttributeC[] = "category";
 static const char displayCategoryElementC[] = "displaycategory";
 static const char featuresRequiredC[] = "featuresRequired";
+static const char featuresPreferredC[] = "featuresPreferred";
 static const char platformIndependentC[] = "platformIndependent";
 static const char fieldPageTitleElementC[] = "fieldpagetitle";
 static const char fieldsElementC[] = "fields";
@@ -472,6 +473,18 @@ static inline FeatureSet requiredFeatures(const QXmlStreamReader &reader)
     return features;
 }
 
+static inline FeatureSet preferredFeatures(const QXmlStreamReader &reader)
+{
+    QString value = reader.attributes().value(QLatin1String(featuresPreferredC)).toString();
+    QStringList stringList = value.split(QLatin1Char(','), QString::SkipEmptyParts);
+    FeatureSet features;
+    foreach (const QString &string, stringList) {
+        Feature feature(Id::fromString(string));
+        features |= feature;
+    }
+    return features;
+}
+
 static inline IWizard::WizardFlags wizardFlags(const QXmlStreamReader &reader)
 {
     IWizard::WizardFlags flags;
@@ -591,6 +604,7 @@ CustomWizardParameters::ParseResult
                     bp->category = attributeValue(reader, categoryAttributeC);
                     bp->kind = kindAttribute(reader);
                     bp->requiredFeatures = requiredFeatures(reader);
+                    bp->preferredFeatures = preferredFeatures(reader);
                     bp->flags = wizardFlags(reader);
                     klass = attributeValue(reader, klassAttributeC);
                     firstPageId = integerAttributeValue(reader, firstPageAttributeC, -1);

src/plugins/qmakeprojectmanager/wizards/qtwizard.cpp
@@ -209,12 +209,17 @@ int BaseQmakeProjectWizardDialog::addModulesPage(int id)
 int BaseQmakeProjectWizardDialog::addTargetSetupPage(int id)
 {
     m_targetSetupPage = new ProjectExplorer::TargetSetupPage;
-    const QString platform = selectedPlatform();
-    Core::FeatureSet features = Core::FeatureSet(QtSupport::Constants::FEATURE_DESKTOP);
-    if (platform.isEmpty())
-        m_targetSetupPage->setPreferredKitMatcher(new QtSupport::QtVersionKitMatcher(features));
-    else
-        m_targetSetupPage->setPreferredKitMatcher(new QtSupport::QtPlatformKitMatcher(platform));
+
+    if (!preferredFeatures().isEmpty()) {
+        m_targetSetupPage->setPreferredKitMatcher(new QtSupport::QtVersionKitMatcher(preferredFeatures()));
+    } else {
+        const QString platform = selectedPlatform();
+        Core::FeatureSet features = Core::FeatureSet(QtSupport::Constants::FEATURE_DESKTOP);
+        if (platform.isEmpty())
+            m_targetSetupPage->setPreferredKitMatcher(new QtSupport::QtVersionKitMatcher(features));
+        else
+            m_targetSetupPage->setPreferredKitMatcher(new QtSupport::QtPlatformKitMatcher(platform));
+    }
 
     m_targetSetupPage->setRequiredKitMatcher(new QtSupport::QtVersionKitMatcher(requiredFeatures()));
 

commit 38928f59f8d5f7b04adbbeda6f74d933e7d41ec5

Fix typo: prefferedFeatures
Introduced with e6cadea29b7588866e5b42ff78f31628cc2e4c76 (Reintroduce
"Jolla: Add preferredFeauters to example template" - non-Mer part)

Note that above mentioned was also ch-p to mer-3.1 as
0da8fb2fd8877ff42f5f0a180cd2ac2c53d56384 (Add preferredFeatures support
to wizards)

src/plugins/projectexplorer/baseprojectwizarddialog.cpp
@@ -71,7 +71,7 @@ BaseProjectWizardDialog::BaseProjectWizardDialog(QWidget *parent,
 {
     setPath(parameters.defaultPath());
     setSelectedPlatform(parameters.selectedPlatform());
-    setPrefferedFeatures(parameters.preferredFeatures());
+    setPreferredFeatures(parameters.preferredFeatures());
     setRequiredFeatures(parameters.requiredFeatures());
     init();
 }
@@ -85,7 +85,7 @@ BaseProjectWizardDialog::BaseProjectWizardDialog(Utils::ProjectIntroPage *introP
 {
     setPath(parameters.defaultPath());
     setSelectedPlatform(parameters.selectedPlatform());
-    setPrefferedFeatures(parameters.preferredFeatures());
+    setPreferredFeatures(parameters.preferredFeatures());
     setRequiredFeatures(parameters.requiredFeatures());
     init();
 }
@@ -216,7 +216,7 @@ Core::FeatureSet BaseProjectWizardDialog::preferredFeatures() const
     return d->preferredFeatureSet;
 }
 
-void BaseProjectWizardDialog::setPrefferedFeatures(const Core::FeatureSet &featureSet)
+void BaseProjectWizardDialog::setPreferredFeatures(const Core::FeatureSet &featureSet)
 {
     d->preferredFeatureSet = featureSet;
 }

src/plugins/projectexplorer/baseprojectwizarddialog.h
@@ -82,7 +82,7 @@ protected:
     void setRequiredFeatures(const Core::FeatureSet &featureSet);
 
     Core::FeatureSet preferredFeatures() const;
-    void setPrefferedFeatures(const Core::FeatureSet &featureSet);
+    void setPreferredFeatures(const Core::FeatureSet &featureSet);
 
 private slots:
     void slotAccepted();