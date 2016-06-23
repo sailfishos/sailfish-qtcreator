sailfishos/sailfish-qtcreator

commit 38bb26d516fa890024188abed5bf645459973620

Mer: Fix singleton pattern implementation & usage

src/plugins/mer/mersdkmanager.cpp
@@ -150,7 +150,7 @@ void MerSdkManager::initialize()
     }
 }
 
-QList<Kit *> MerSdkManager::merKits() const
+QList<Kit *> MerSdkManager::merKits()
 {
     QList<Kit*> kits;
     foreach (Kit *kit, KitManager::kits()) {
@@ -192,9 +192,9 @@ MerSdkManager *MerSdkManager::instance()
     return m_instance;
 }
 
-QList<MerSdk*> MerSdkManager::sdks() const
+QList<MerSdk*> MerSdkManager::sdks()
 {
-    return m_sdks.values();
+    return m_instance->m_sdks.values();
 }
 
 const FileName& MerSdkManager::checkInstallLocation(const FileName &local,
@@ -278,7 +278,7 @@ QList<MerSdk*> MerSdkManager::restoreSdks(const FileName &fileName)
         return result;
     }
 
-    m_installDir = data.value(QLatin1String(MER_SDK_INSTALLDIR)).toString();
+    m_instance->m_installDir = data.value(QLatin1String(MER_SDK_INSTALLDIR)).toString();
 
     int count = data.value(QLatin1String(MER_SDK_COUNT_KEY), 0).toInt();
     for (int i = 0; i < count; ++i) {
@@ -297,13 +297,13 @@ QList<MerSdk*> MerSdkManager::restoreSdks(const FileName &fileName)
     return result;
 }
 
-void MerSdkManager::storeSdks() const
+void MerSdkManager::storeSdks()
 {
     QVariantMap data;
     data.insert(QLatin1String(MER_SDK_FILE_VERSION_KEY), 1);
     data.insert(QLatin1String(MER_SDK_INSTALLDIR), m_installDir);
     int count = 0;
-    foreach (const MerSdk* sdk, m_sdks) {
+    foreach (const MerSdk* sdk, m_instance->m_sdks) {
         if (!sdk->isValid()) {
             qWarning() << sdk->virtualMachineName() << "is configured incorrectly...";
         }
@@ -314,7 +314,7 @@ void MerSdkManager::storeSdks() const
         ++count;
     }
     data.insert(QLatin1String(MER_SDK_COUNT_KEY), count);
-    m_writer->save(data, ICore::mainWindow());
+    m_instance->m_writer->save(data, ICore::mainWindow());
 }
 
 bool MerSdkManager::isMerKit(const Kit *kit)
@@ -444,39 +444,39 @@ bool MerSdkManager::authorizePublicKey(const QString &authorizedKeysPath,
     return success;
 }
 
-bool MerSdkManager::hasSdk(const MerSdk* sdk) const
+bool MerSdkManager::hasSdk(const MerSdk* sdk)
 {
-    return m_sdks.contains(sdk->virtualMachineName());
+    return m_instance->m_sdks.contains(sdk->virtualMachineName());
 }
 
 // takes ownership
 void MerSdkManager::addSdk(MerSdk *sdk)
 {
-    if (m_sdks.contains(sdk->virtualMachineName()))
+    if (m_instance->m_sdks.contains(sdk->virtualMachineName()))
         return;
-    m_sdks.insert(sdk->virtualMachineName(), sdk);
+    m_instance->m_sdks.insert(sdk->virtualMachineName(), sdk);
     connect(sdk, &MerSdk::targetsChanged,
-            this, &MerSdkManager::sdksUpdated);
+            m_instance, &MerSdkManager::sdksUpdated);
     connect(sdk, &MerSdk::privateKeyChanged,
-            this, &MerSdkManager::sdksUpdated);
+            m_instance, &MerSdkManager::sdksUpdated);
     connect(sdk, &MerSdk::headlessChanged,
-            this, &MerSdkManager::sdksUpdated);
+            m_instance, &MerSdkManager::sdksUpdated);
     sdk->attach();
-    emit sdksUpdated();
+    emit m_instance->sdksUpdated();
 }
 
 // pass back the ownership
 void MerSdkManager::removeSdk(MerSdk *sdk)
 {
-    if (!m_sdks.contains(sdk->virtualMachineName()))
+    if (!m_instance->m_sdks.contains(sdk->virtualMachineName()))
         return;
-    m_sdks.remove(sdk->virtualMachineName());
+    m_instance->m_sdks.remove(sdk->virtualMachineName());
     disconnect(sdk, &MerSdk::targetsChanged,
-               this, &MerSdkManager::sdksUpdated);
+               m_instance, &MerSdkManager::sdksUpdated);
     disconnect(sdk, &MerSdk::privateKeyChanged,
-               this, &MerSdkManager::sdksUpdated);
+               m_instance, &MerSdkManager::sdksUpdated);
     sdk->detach();
-    emit sdksUpdated();
+    emit m_instance->sdksUpdated();
 }
 
 //ownership passed to caller
@@ -508,9 +508,9 @@ MerSdk* MerSdkManager::createSdk(const QString &vmName)
 }
 
 
-MerSdk* MerSdkManager::sdk(const QString &sdkName) const
+MerSdk* MerSdkManager::sdk(const QString &sdkName)
 {
-    return m_sdks.value(sdkName);
+    return m_instance->m_sdks.value(sdkName);
 }
 
 bool MerSdkManager::validateKit(const Kit *kit)
@@ -653,10 +653,9 @@ void MerSdkManager::updateDevices()
 
 void MerPlugin::verifyTargets(const QString &vm, QStringList expectedKits, QStringList expectedToolChains, QStringList expectedQtVersions)
 {
-    MerSdkManager *sdkManager = MerSdkManager::instance();
-    QList<Kit*> kits = sdkManager->merKits();
-    QList<MerToolChain*> toolchains = sdkManager->merToolChains();
-    QList<MerQtVersion*> qtversions = sdkManager->merQtVersions();
+    QList<Kit*> kits = MerSdkManager::merKits();
+    QList<MerToolChain*> toolchains = MerSdkManager::merToolChains();
+    QList<MerQtVersion*> qtversions = MerSdkManager::merQtVersions();
 
     foreach (Kit* x, kits) {
         QString target = MerSdkManager::targetNameForKit(x);
@@ -739,11 +738,8 @@ void MerPlugin::testMerSdkManager()
 
     QVERIFY2(QFile::copy(initFile, targetFile), "Could not copy init.xml to target.xml");
 
-    MerSdkManager *sdkManager = MerSdkManager::instance();
-
-    QVERIFY(sdkManager);
-    QVERIFY(sdkManager->sdks().isEmpty());
-    MerSdk *sdk = sdkManager->createSdk(virtualMachine);
+    QVERIFY(MerSdkManager::sdks().isEmpty());
+    MerSdk *sdk = MerSdkManager::createSdk(virtualMachine);
     QVERIFY(sdk);
     QVERIFY(!sdk->isValid());
 
@@ -752,9 +748,9 @@ void MerPlugin::testMerSdkManager()
     sdk->setSharedTargetsPath(QDir::toNativeSeparators(filepath));
 
     QVERIFY(sdk->isValid());
-    sdkManager->addSdk(sdk);
+    MerSdkManager::addSdk(sdk);
 
-    QVERIFY(!sdkManager->sdks().isEmpty());
+    QVERIFY(!MerSdkManager::sdks().isEmpty());
 
     verifyTargets(virtualMachine, initKits, initToolChains, initQtVersions);
 
@@ -768,16 +764,16 @@ void MerPlugin::testMerSdkManager()
 
     verifyTargets(virtualMachine, expectedKits, expectedToolChains, expectedQtVersions);
 
-    sdkManager->removeSdk(sdk);
+    MerSdkManager::removeSdk(sdk);
 
-    QList<Kit*> kits = sdkManager->merKits();
-    QList<MerToolChain*> toolchains = sdkManager->merToolChains();
-    QList<MerQtVersion*> qtversions = sdkManager->merQtVersions();
+    QList<Kit*> kits = MerSdkManager::merKits();
+    QList<MerToolChain*> toolchains = MerSdkManager::merToolChains();
+    QList<MerQtVersion*> qtversions = MerSdkManager::merQtVersions();
 
     QVERIFY2(kits.isEmpty(), "Merkit not removed");
     QVERIFY2(toolchains.isEmpty(), "Toolchain not removed");
     QVERIFY2(qtversions.isEmpty(), "QtVersion not removed");
-    QVERIFY(sdkManager->sdks().isEmpty());
+    QVERIFY(MerSdkManager::sdks().isEmpty());
     //cleanup
 
     QVERIFY2(QFile::remove(targetFile), "Could not remove target.xml");

src/plugins/mer/mersdkmanager.h
@@ -63,18 +63,18 @@ public:
     static bool generateSshKey(const QString &privKeyPath, QString &error);
 
     ~MerSdkManager() override;
-    QList<MerSdk*> sdks() const;
-    MerSdk* sdk(const QString &virtualMachineName) const;
-    MerSdk* createSdk(const QString &vmName);
-    void addSdk(MerSdk *sdk);
-    void removeSdk(MerSdk *sdk);
-    void restoreSdks();
-    bool hasSdk(const MerSdk *sdk) const;
+    static QList<MerSdk*> sdks();
+    static MerSdk* sdk(const QString &virtualMachineName);
+    static MerSdk* createSdk(const QString &vmName);
+    static void addSdk(MerSdk *sdk);
+    static void removeSdk(MerSdk *sdk);
+    static void restoreSdks();
+    static bool hasSdk(const MerSdk *sdk);
 
-    QList<ProjectExplorer::Kit*> merKits() const;
+    static QList<ProjectExplorer::Kit*> merKits();
 
 public slots:
-    void storeSdks() const;
+    void storeSdks();
 
 signals:
     void sdksUpdated();

src/plugins/mer/merconnectionmanager.cpp
@@ -372,8 +372,7 @@ void MerConnectionManager::update()
     m_sdkAction->update();
 }
 
-QString MerConnectionManager::testConnection(const SshConnectionParameters &params,
-        bool *ok) const
+QString MerConnectionManager::testConnection(const SshConnectionParameters &params, bool *ok)
 {
     SshConnectionParameters p = params;
     SshConnection connection(p);

src/plugins/mer/merconnectionmanager.h
@@ -49,7 +49,7 @@ class MerConnectionManager : public QObject
 public:
     static MerConnectionManager* instance();
     ~MerConnectionManager() override;
-    QString testConnection(const QSsh::SshConnectionParameters &params, bool *ok = 0) const;
+    static QString testConnection(const QSsh::SshConnectionParameters &params, bool *ok = 0);
 private slots:
     void update();
     void handleStartupProjectChanged(ProjectExplorer::Project *project);

src/plugins/mer/meremulatormodedialog.cpp
@@ -57,9 +57,6 @@ MerEmulatorModeDialog::MerEmulatorModeDialog(QObject *parent)
       m_kit(0),
       m_emulator(0)
 {
-    auto sessionManager = static_cast<SessionManager *>(SessionManager::instance());
-    auto kitManager = static_cast<KitManager *>(KitManager::instance());
-
     m_action->setEnabled(false);
     m_action->setText(tr("&Emulator mode..."));
 
@@ -67,10 +64,10 @@ MerEmulatorModeDialog::MerEmulatorModeDialog(QObject *parent)
             this, &MerEmulatorModeDialog::execDialog);
 
     onStartupProjectChanged(SessionManager::startupProject());
-    connect(sessionManager, &SessionManager::startupProjectChanged,
+    connect(SessionManager::instance(), &SessionManager::startupProjectChanged,
             this, &MerEmulatorModeDialog::onStartupProjectChanged);
 
-    connect(kitManager, &KitManager::kitUpdated,
+    connect(KitManager::instance(), &KitManager::kitUpdated,
             this, &MerEmulatorModeDialog::onKitUpdated);
 }
 

src/plugins/mer/merhardwaredevicewizardpages.cpp
@@ -137,7 +137,7 @@ void MerHardwareDeviceWizardSelectionPage::handleTestConnectionClicked()
     sshParams.password = password();
 
     m_ui->connectionLabelEdit->setText(tr("Connecting to machine %1 ...").arg(hostName()));
-    m_ui->connectionLabelEdit->setText(MerConnectionManager::instance()->testConnection(sshParams,
+    m_ui->connectionLabelEdit->setText(MerConnectionManager::testConnection(sshParams,
                 &m_connectionTestOk));
     if (!m_connectionTestOk)
         goto end;
@@ -262,7 +262,7 @@ MerHardwareDeviceWizardSetupPage::MerHardwareDeviceWizardSetupPage(QWidget *pare
     m_ui->sshCheckBox->setChecked(true);
 
     static QRegExp regExp(tr("MerSDK"));
-    QList<MerSdk*> sdks = MerSdkManager::instance()->sdks();
+    QList<MerSdk*> sdks = MerSdkManager::sdks();
     foreach (const MerSdk *s, sdks) {
         m_ui->merSdkComboBox->addItem(s->virtualMachineName());
         if (regExp.indexIn(s->virtualMachineName()) != -1) {
@@ -311,7 +311,7 @@ QString MerHardwareDeviceWizardSetupPage::freePorts() const
 
 void MerHardwareDeviceWizardSetupPage::handleSdkVmChanged(const QString &vmName)
 {
-    MerSdk* sdk = MerSdkManager::instance()->sdk(vmName);
+    MerSdk* sdk = MerSdkManager::sdk(vmName);
     QTC_ASSERT(sdk,return);
     const MerHardwareDeviceWizard* wizard = qobject_cast<MerHardwareDeviceWizard*>(this->wizard());
     QTC_ASSERT(wizard,return);
@@ -341,7 +341,7 @@ bool MerHardwareDeviceWizardSetupPage::isNewSshKeysRquired() const
 
 QString MerHardwareDeviceWizardSetupPage::sharedSshPath() const
 {
-    MerSdk* sdk = MerSdkManager::instance()->sdk(m_ui->merSdkComboBox->currentText());
+    MerSdk* sdk = MerSdkManager::sdk(m_ui->merSdkComboBox->currentText());
     QTC_ASSERT(sdk,return QString());
     return sdk->sharedConfigPath();
 }

src/plugins/mer/mermanagementwebview.cpp
@@ -186,7 +186,7 @@ void MerManagementWebViewSdksModel::onSdksUpdated()
 {
     QList<MerSdk *> oldSdks = m_sdks;
 
-    m_sdks = MerSdkManager::instance()->sdks();
+    m_sdks = MerSdkManager::sdks();
 
     if (m_sdks != oldSdks) {
         beginReset();

src/plugins/mer/meroptionswidget.cpp
@@ -89,7 +89,7 @@ MerOptionsWidget::~MerOptionsWidget()
     delete m_ui;
 
     // Destroy newly created but not-applied SDKs
-    QSet<MerSdk *> currentSdks = MerSdkManager::instance()->sdks().toSet();
+    QSet<MerSdk *> currentSdks = MerSdkManager::sdks().toSet();
     foreach (MerSdk *sdk, m_sdks) {
         if (!currentSdks.contains(sdk)) {
             delete sdk;
@@ -119,8 +119,7 @@ QString MerOptionsWidget::searchKeyWordMatchString() const
 void MerOptionsWidget::store()
 {
     QMap<QString, MerSdk*> sdks = m_sdks;
-    MerSdkManager *sdkManager = MerSdkManager::instance();
-    QList<MerSdk*> currentSdks = sdkManager->sdks();
+    QList<MerSdk*> currentSdks = MerSdkManager::sdks();
 
     foreach (MerSdk *sdk, sdks) {
         if (m_sshPrivKeys.contains(sdk))
@@ -133,7 +132,7 @@ void MerOptionsWidget::store()
 
     foreach (MerSdk *sdk, currentSdks) {
         if (!sdks.contains(sdk->virtualMachineName())) {
-            sdkManager->removeSdk(sdk);
+            MerSdkManager::removeSdk(sdk);
             delete sdk;
         } else {
             sdks.remove(sdk->virtualMachineName());
@@ -141,7 +140,7 @@ void MerOptionsWidget::store()
     }
 
     foreach (MerSdk *sdk,sdks)
-        sdkManager->addSdk(sdk);
+        MerSdkManager::addSdk(sdk);
 
     m_sshPrivKeys.clear();
     m_sshTimeout.clear();
@@ -167,7 +166,7 @@ void MerOptionsWidget::onAddButtonClicked()
     if (m_sdks.contains(dialog.selectedSdkName()))
         return;
 
-    MerSdk* sdk = MerSdkManager::instance()->createSdk(dialog.selectedSdkName());
+    MerSdk* sdk = MerSdkManager::createSdk(dialog.selectedSdkName());
     m_sdks[sdk->virtualMachineName()] = sdk;
     m_virtualMachine = sdk->virtualMachineName();
     update();
@@ -181,7 +180,7 @@ void MerOptionsWidget::onRemoveButtonClicked()
 
     if (m_sdks.contains(vmName)) {
          MerSdk *removed = m_sdks.take(vmName);
-         QList<MerSdk *> currentSdks = MerSdkManager::instance()->sdks();
+         QList<MerSdk *> currentSdks = MerSdkManager::sdks();
          if (!currentSdks.contains(removed))
              delete removed;
          if (!m_sdks.isEmpty())
@@ -201,7 +200,7 @@ void MerOptionsWidget::onTestConnectionButtonClicked()
             params.privateKeyFile = m_sshPrivKeys[sdk];
         m_ui->sdkDetailsWidget->setStatus(tr("Connecting to machine %1 ...").arg(sdk->virtualMachineName()));
         m_ui->sdkDetailsWidget->setTestButtonEnabled(false);
-        m_status = MerConnectionManager::instance()->testConnection(params);
+        m_status = MerConnectionManager::testConnection(params);
         m_ui->sdkDetailsWidget->setTestButtonEnabled(true);
         update();
     } else {
@@ -220,7 +219,7 @@ void MerOptionsWidget::onAuthorizeSshKey(const QString &file)
                + QLatin1Char('/') + QLatin1String(Constants::MER_AUTHORIZEDKEYS_FOLDER);
     foreach (const QString &path, authorizedKeysPaths) {
         QString error;
-        const bool success = MerSdkManager::instance()->authorizePublicKey(path, pubKeyPath, error);
+        const bool success = MerSdkManager::authorizePublicKey(path, pubKeyPath, error);
         if (!success)
             QMessageBox::critical(this, tr("Cannot Authorize Keys"), error);
         else
@@ -249,10 +248,9 @@ void MerOptionsWidget::onGenerateSshKey(const QString &privKeyPath)
 
 void MerOptionsWidget::onSdksUpdated()
 {
-    MerSdkManager *sdkManager = MerSdkManager::instance();
     m_sdks.clear();
     m_virtualMachine.clear();
-    foreach (MerSdk *sdk, sdkManager->sdks()) {
+    foreach (MerSdk *sdk, MerSdkManager::sdks()) {
         m_sdks[sdk->virtualMachineName()] = sdk;
         m_virtualMachine = sdk->virtualMachineName();
     }

commit eec59af9f62497948aca47546a1c10cb4adf6b36

Mer: Fix build with tests enabled
Broken since 38bb26d516fa890024188abed5bf645459973620 (Mer: Fix
singleton pattern implementation & usage)

src/plugins/mer/mersdkmanager.cpp
@@ -160,7 +160,7 @@ QList<Kit *> MerSdkManager::merKits()
     return kits;
 }
 
-QList<MerToolChain *> MerSdkManager::merToolChains() const
+QList<MerToolChain *> MerSdkManager::merToolChains()
 {
     QList<MerToolChain*> toolchains;
     foreach (ToolChain *toolchain, ToolChainManager::toolChains()) {
@@ -173,7 +173,7 @@ QList<MerToolChain *> MerSdkManager::merToolChains() const
     return toolchains;
 }
 
-QList<MerQtVersion *> MerSdkManager::merQtVersions() const
+QList<MerQtVersion *> MerSdkManager::merQtVersions()
 {
     QList<MerQtVersion*> qtversions;
     foreach (BaseQtVersion *qtVersion, QtVersionManager::versions()) {

src/plugins/mer/mersdkmanager.h
@@ -88,8 +88,8 @@ private:
     MerSdkManager();
     void restore();
     QList<MerSdk*> restoreSdks(const Utils::FileName &fileName);
-    QList<MerToolChain*> merToolChains() const;
-    QList<MerQtVersion*> merQtVersions() const;
+    static QList<MerToolChain*> merToolChains();
+    static QList<MerQtVersion*> merQtVersions();
     const Utils::FileName& checkInstallLocation(const Utils::FileName &l, const Utils::FileName &g);
 private:
     static MerSdkManager *m_instance;