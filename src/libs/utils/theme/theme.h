ff5e7823d8e1b493cc765f086350cbaf7a5fc12a
Jarkko Lehtoranta <jarkko.lehtoranta@jolla.com>
2016-09-22 06:36:27 +0300

    Define an active icon color for Mer mode
    (cherry picked from commit 04bcb91154a953a8ff5b0144b45c7f834ac4b4ee)

--- src/libs/utils/theme/theme.h
+++ src/libs/utils/theme/theme.h
@@ -198,6 +198,7 @@ public:
         IconsModeProjectActiveColor,
         IconsModeAnalyzeActiveColor,
         IconsModeHelpActiveColor,
+        IconsModeMerActiveColor,
 
         /* Code model Icons */
 
