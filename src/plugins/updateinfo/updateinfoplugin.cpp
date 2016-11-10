qt-creator/qt-creator

commit 26ba9e62f36871c71df8ed54509d22b9c19d0c56

UpdateInfo: Fix checking for updates
Checking for updates failed when MaintenanceTool printed out log
messages. MaintenanceTool is expected to print out only xml data. So,
parsing the output failed.

MaintenanceTool printed out warnings about SSL and Qt account.

Logging is now disabled when running MaintenanceTool.

Change-Id: Ib163dae81368c8fec7218e5961590e07c208a73e
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/updateinfo/updateinfoplugin.cpp
@@ -123,7 +123,9 @@ void UpdateInfoPlugin::startCheckForUpdates()
 {
     stopCheckForUpdates();
 
-    d->m_checkUpdatesCommand = new ShellCommand(QString(), QProcessEnvironment());
+    QProcessEnvironment env;
+    env.insert("QT_LOGGING_RULES", "*=false");
+    d->m_checkUpdatesCommand = new ShellCommand(QString(), env);
     connect(d->m_checkUpdatesCommand, &ShellCommand::stdOutText, this, &UpdateInfoPlugin::collectCheckForUpdatesOutput);
     connect(d->m_checkUpdatesCommand, &ShellCommand::finished, this, &UpdateInfoPlugin::checkForUpdatesFinished);
     d->m_checkUpdatesCommand->addJob(Utils::FileName(QFileInfo(d->m_maintenanceTool)), QStringList(QLatin1String("--checkupdates")));

sailfishos/sailfish-qtcreator

commit cb3b103cc6efe757331bfa5ec4d86f2cf78ad690

UpdateInfo: Inherit environment when checking updates
Broken since 26ba9e62f36871c71df8ed54509d22b9c19d0c56 (UpdateInfo: Fix
checking for updates)

src/plugins/updateinfo/updateinfoplugin.cpp
@@ -141,7 +141,7 @@ void UpdateInfoPlugin::startCheckForUpdates()
 {
     stopCheckForUpdates();
 
-    QProcessEnvironment env;
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
     env.insert(QLatin1String("QT_LOGGING_RULES"), QLatin1String("*=false"));
     d->m_checkUpdatesCommand = new ShellCommand(QString(), env);
     connect(d->m_checkUpdatesCommand, &ShellCommand::stdOutText, this, &UpdateInfoPlugin::collectCheckForUpdatesOutput);