5341e086cb7c65b570b3cfcad7015c5ef4286cff
Martin Kampas <martin.kampas@jolla.com>
2017-02-10 12:32:57 +0100

    Mer: Properly reload management web view after VM start
    This applies when the build engine VM is started some other way than
    using the "Start the build engine!" link inside the web management view.

--- src/plugins/mer/mermanagementwebview.cpp
+++ src/plugins/mer/mermanagementwebview.cpp
@@ -261,7 +261,7 @@ void MerManagementWebView::resetWebView()
 
     if (m_selectedSdk) {
         disconnect(m_selectedSdk->connection(), &MerConnection::virtualMachineOffChanged,
-                ui->webView, &QWebView::reload);
+                this, &MerManagementWebView::resetWebView);
     }
 
     m_selectedSdk = m_sdksModel->sdkAt(ui->sdksComboBox->currentIndex());
@@ -270,7 +270,7 @@ void MerManagementWebView::resetWebView()
         url = QUrl(QLatin1String(CONTROLCENTER_URL_BASE));
         url.setPort(m_selectedSdk->wwwPort());
         connect(m_selectedSdk->connection(), &MerConnection::virtualMachineOffChanged,
-                ui->webView, &QWebView::reload);
+                this, &MerManagementWebView::resetWebView);
     } else {
         url = QLatin1String("about:blank");
     }
