0dc6e4544527682f04a52098f78cf1ffc6bc9cee
Martin Kampas <martin.kampas@jolla.com>
2017-03-23 15:08:56 +0100

    QmakeProjectManager: Suppress warning on tool chain mismatch
    This is to suppres warnings like:
    
    warning: "/usr/bin/g++" is used by qmake, but
    "~/.config/SailfishOS-SDK/mer-sdk-tools/MerSDK/SailfishOS-i486/gcc" is
    configured in the kit.  Please update your kit or choose a mkspec for
    qmake that matches your target environment better.
    
    (cherry picked from commit a3464257fe42652039a4bf126d9a26b928a29cc1)

--- src/plugins/qmakeprojectmanager/qmakeproject.cpp
+++ src/plugins/qmakeprojectmanager/qmakeproject.cpp
@@ -1313,6 +1313,7 @@ void QmakeProject::testToolChain(ToolChain *tc, const Utils::FileName &path) con
     }
 
     if (!env.isSameExecutable(path.toString(), expected.toString())) {
+#if 0 // Does not work with Mer SDK
         const QPair<Utils::FileName, Utils::FileName> pair = qMakePair(expected, path);
         if (!m_toolChainWarnings.contains(pair)) {
             // Suppress warnings on Apple machines where compilers in /usr/bin point into Xcode.
@@ -1328,6 +1329,7 @@ void QmakeProject::testToolChain(ToolChain *tc, const Utils::FileName &path) con
                 m_toolChainWarnings.insert(pair);
             }
         }
+#endif
     }
 }
 
