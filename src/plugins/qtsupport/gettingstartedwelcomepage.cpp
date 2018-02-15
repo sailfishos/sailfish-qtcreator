e3eaf0be1036dd1eb949738d3025f56a14fafb08
Martin Kampas <martin.kampas@jolla.com>
2018-02-15 12:42:17 +0100

    QtSupport: Copy examples to a writable location unconditionally
    This would cause a build error with Sailfish/Mer projects where the SDK
    installation directory is usually out of the configured projects
    directory.
    
    (cherry picked from commit a977dd8c24090fe579983e503cfac75cbe5d49d6)

--- src/plugins/qtsupport/gettingstartedwelcomepage.cpp
+++ src/plugins/qtsupport/gettingstartedwelcomepage.cpp
@@ -167,6 +167,7 @@ void ExamplesWelcomePage::openProject(const ExampleItem &item)
     if (!proFileInfo.exists())
         return;
 
+#if 0
     // If the Qt is a distro Qt on Linux, it will not be writable, hence compilation will fail
     // Same if it is installed in non-writable location for other reasons
     const bool needsCopy = withNtfsPermissions<bool>([proFileInfo] {
@@ -176,6 +177,7 @@ void ExamplesWelcomePage::openProject(const ExampleItem &item)
                 || !QFileInfo(pathInfo.path()).isWritable() /* shadow build directory */;
     });
     if (needsCopy)
+#endif
         proFile = copyToAlternativeLocation(proFileInfo, filesToOpen, item.dependencies);
 
     // don't try to load help and files if loading the help request is being cancelled
