sailfishos/sailfish-qtcreator

commit 5a287c6ad1c5ba029e38d90d3596bd7c48b5b462

Mer: Use device ID to locate SSH key for emulator. Contribute to JB#37138

src/plugins/mer/meremulatordevice.cpp
@@ -363,7 +363,7 @@ void MerEmulatorDevice::generateSshKey(const QString& user) const
         QString index(QLatin1String("/ssh/private_keys/%1/"));
         //TODO fix me:
         QString privateKeyFile = m_sharedConfigPath +
-                index.arg(virtualMachine()).replace(QLatin1Char(' '), QLatin1Char('_')) + user;
+                index.arg(id().toString()).replace(QLatin1Char(' '), QLatin1Char('_')) + user;
         PublicKeyDeploymentDialog dialog(privateKeyFile, virtualMachine(),
                                          user, sharedSshPath(), ICore::dialogParent());
 
@@ -378,7 +378,7 @@ SshConnectionParameters MerEmulatorDevice::sshParametersForUser(const SshConnect
     QString index(QLatin1String("/ssh/private_keys/%1/"));
     //TODO fix me:
     QString privateKeyFile = sharedConfigPath()  +
-            index.arg(virtualMachine()).replace(QLatin1Char(' '), QLatin1Char('_')) + user;
+            index.arg(id().toString()).replace(QLatin1Char(' '), QLatin1Char('_')) + user;
     SshConnectionParameters m_sshParams = sshParams;
     m_sshParams.userName = user;
     m_sshParams.privateKeyFile = privateKeyFile;

commit c63160233b2a7b2310809d07b809af39c0cfcdb2

Mer: Fix initial connection failure when adding emulator
This bug was introduced with commit
5a287c6ad1c5ba029e38d90d3596bd7c48b5b462 (Mer: Use device ID to locate
SSH key for emulator. Contribute to JB#37138) - MerEmualtorSshPage was
not updated to use device ID instead of VM name.

src/plugins/mer/merdevicefactory.cpp
@@ -103,7 +103,7 @@ IDevice::Ptr MerDeviceFactory::create(Core::Id id) const
         sshParams.privateKeyFile = wizard.userPrivateKey();
 
         //hardcoded values requested by customer;
-        MerEmulatorDevice::Ptr device = MerEmulatorDevice::create();
+        MerEmulatorDevice::Ptr device = MerEmulatorDevice::create(wizard.emulatorId());
         device->setVirtualMachine(wizard.emulatorVm());
         device->setMac(wizard.mac());
         device->setSubnet(QLatin1String("10.220.220"));

src/plugins/mer/meremulatordevice.cpp
@@ -184,9 +184,9 @@ private:
 };
 
 
-MerEmulatorDevice::Ptr MerEmulatorDevice::create()
+MerEmulatorDevice::Ptr MerEmulatorDevice::create(Core::Id id)
 {
-    return Ptr(new MerEmulatorDevice());
+    return Ptr(new MerEmulatorDevice(id));
 }
 
 
@@ -195,8 +195,8 @@ IDevice::Ptr MerEmulatorDevice::clone() const
     return Ptr(new MerEmulatorDevice(*this));
 }
 
-MerEmulatorDevice::MerEmulatorDevice():
-    MerDevice(QString(), Emulator, ManuallyAdded, Core::Id())
+MerEmulatorDevice::MerEmulatorDevice(Core::Id id)
+    : MerDevice(QString(), Emulator, ManuallyAdded, id)
     , m_connection(new MerConnection(0 /* not bug */))
     , m_orientation(Qt::Vertical)
     , m_viewScaled(false)

src/plugins/mer/meremulatordevice.h
@@ -73,7 +73,7 @@ public:
     typedef QSharedPointer<MerEmulatorDevice> Ptr;
     typedef QSharedPointer<const MerEmulatorDevice> ConstPtr;
 
-    static Ptr create();
+    static Ptr create(Core::Id id = Core::Id());
     ProjectExplorer::IDevice::Ptr clone() const;
     ~MerEmulatorDevice() override;
 
@@ -119,7 +119,7 @@ public:
     void updateConnection();
 
 private:
-    MerEmulatorDevice();
+    MerEmulatorDevice(Core::Id id);
     MerEmulatorDevice(const MerEmulatorDevice &other);
 
     void updateAvailableDeviceModels();

src/plugins/mer/meremulatordevicewizard.cpp
@@ -22,6 +22,8 @@
 
 #include "meremulatordevicewizard.h"
 
+#include <coreplugin/id.h>
+
 #include "mersdkmanager.h"
 
 namespace Mer {
@@ -33,6 +35,11 @@ MerEmulatorDeviceWizard::MerEmulatorDeviceWizard(QWidget *parent): QWizard(paren
     addPage(&m_vmPage);
     addPage(&m_sshPage);
     m_sshPage.setCommitPage(true);
+}
+
+Core::Id MerEmulatorDeviceWizard::emulatorId() const
+{
+    return Core::Id::fromString(m_vmPage.emulatorVm());
 }
 
 QString MerEmulatorDeviceWizard::configName() const

src/plugins/mer/meremulatordevicewizard.h
@@ -27,6 +27,10 @@
 
 #include <QWizard>
 
+namespace Core {
+    class Id;
+}
+
 namespace Mer {
 namespace Internal {
 
@@ -36,6 +40,7 @@ class MerEmulatorDeviceWizard : public QWizard
 public:
     explicit MerEmulatorDeviceWizard(QWidget *parent = 0);
 
+    Core::Id emulatorId() const;
     QString configName() const;
     QString userName() const;
     QString rootName() const;

src/plugins/mer/meremulatordevicewizardpages.cpp
@@ -195,7 +195,7 @@ void MerEmualtorSshPage::initializePage()
    QTC_ASSERT(wizard,return);
    //TODO: fix me
    QString sshKeyPath(QDir::toNativeSeparators(wizard->sharedConfigPath() +
-                      index.arg(wizard->emulatorVm()).replace(QLatin1Char(' '),QLatin1Char('_')) +
+                      index.arg(wizard->emulatorId().toString()).replace(QLatin1Char(' '), QLatin1Char('_')) +
                       QLatin1String("%1")));
    if(!wizard->sharedConfigPath().isEmpty()) {
        m_ui->userSshKeyLabelEdit->setText(sshKeyPath.arg(userName()));