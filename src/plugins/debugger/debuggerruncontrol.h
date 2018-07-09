a2a1a4be942cd5b66b9cc56905ce1cabc032395e
Martin Kampas <martin.kampas@jolla.com>
2018-07-09 16:55:13 +0200

    Debugger: Add DebuggerRunControl::addSourcePathMap

--- src/plugins/debugger/debuggerruncontrol.h
+++ src/plugins/debugger/debuggerruncontrol.h
@@ -80,6 +80,8 @@ public:
     void setSolibSearchPath(const QStringList &list);
     void addSolibSearchDir(const QString &str);
 
+    void addSourcePathMap(const QString &installPath, const QString &buildPath);
+
     static void setBreakOnMainNextTime();
 
     void setInferior(const ProjectExplorer::Runnable &runnable);
