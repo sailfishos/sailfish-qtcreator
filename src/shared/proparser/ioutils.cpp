d6b2256cf1386a42b29dc6c02add7329b0c77b2c
Martin Kampas <martin.kampas@jolla.com>
2018-09-20 06:26:25 +0200

    Revert "qmake: require a drive in a DOS path for it to be absolute"
    This reverts commit 912dd8df64c05fcdcbc9d862a41052654dd07546.
    
    The commit broke code completion on Windows hosts because it does not
    consider path conventions on the target platform. Here the target
    platform is Unix-like. Having an absolute path specified in a project
    file, e.g. with INCLUDEPATHS += /usr/include/foo, it would be considered
    as relative path on Windows hosts and prefixed with build directory path
    as a fallback method to get an absolute path from it. Instead it should
    be treated as an absolute path, in which case it would be later prefixed
    with sysroot path, which is the desired behavior.
    
    (cherry picked from commit 71ea6aec9b58ea099fae9e0a6f10ccdab6cb0a12)

--- src/shared/proparser/ioutils.cpp
+++ src/shared/proparser/ioutils.cpp
@@ -64,22 +64,21 @@ IoUtils::FileType IoUtils::fileType(const QString &fileName)
 
 bool IoUtils::isRelativePath(const QString &path)
 {
+    if (path.startsWith(QLatin1Char('/')))
+        return false;
 #ifdef QMAKE_BUILTIN_PRFS
     if (path.startsWith(QLatin1String(":/")))
         return false;
 #endif
 #ifdef Q_OS_WIN
-    // Unlike QFileInfo, this considers only paths with both a drive prefix and
-    // a subsequent (back-)slash absolute:
+    if (path.startsWith(QLatin1Char('\\')))
+        return false;
+    // Unlike QFileInfo, this won't accept a relative path with a drive letter.
+    // Such paths result in a royal mess anyway ...
     if (path.length() >= 3 && path.at(1) == QLatin1Char(':') && path.at(0).isLetter()
-        && (path.at(2) == QLatin1Char('/') || path.at(2) == QLatin1Char('\\'))) {
+        && (path.at(2) == QLatin1Char('/') || path.at(2) == QLatin1Char('\\')))
         return false;
-    }
-    // (... unless, of course, they're UNC, which qmake fails on anyway)
-#else
-    if (path.startsWith(QLatin1Char('/')))
-        return false;
-#endif // Q_OS_WIN
+#endif
     return true;
 }
 
@@ -99,12 +98,6 @@ QString IoUtils::resolvePath(const QString &baseDir, const QString &fileName)
         return QString();
     if (isAbsolutePath(fileName))
         return QDir::cleanPath(fileName);
-#ifdef Q_OS_WIN // Add drive to otherwise-absolute path:
-    if (fileName.at(0).unicode() == '/' || fileName.at(0).unicode() == '\\') {
-        Q_ASSERT(isAbsolutePath(baseDir));
-        return QDir::cleanPath(baseDir.left(2) + fileName);
-    }
-#endif // Q_OS_WIN
     return QDir::cleanPath(baseDir + QLatin1Char('/') + fileName);
 }
 
