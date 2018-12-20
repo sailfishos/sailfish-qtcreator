b9508cb01d637dad26ef204413cc5ec090e7a9ef
Jarkko Lehtoranta <jarkko.lehtoranta@jolla.com>
2018-12-20 20:02:38 +0200

    Debugger: Hide/Show debug button based on project status
    (cherry picked from commit 2b53df44cada3fc91d923ec1f9b0b569adbcedd7 and
    commit 29634ff83b7cdcd5ee918c0d5b724ee9fe6d78c3)

--- src/plugins/debugger/debuggerplugin.cpp
+++ src/plugins/debugger/debuggerplugin.cpp
@@ -1477,6 +1477,10 @@ void DebuggerPluginPrivate::updatePresetState()
         m_startAction.setIcon(startIcon(true));
         m_startAction.setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
         m_startAction.setVisible(true);
+        if (!BuildManager::isBuilding()) {
+            m_startAction.setVisible(!SessionManager::startupProject()
+                    || SessionManager::startupProject()->needsConfiguration() || canRun);
+        }
         m_debugWithoutDeployAction.setEnabled(canRun);
         m_visibleStartAction.setAction(&m_startAction);
         m_hiddenStopAction.setAction(&m_undisturbableAction);
@@ -1510,6 +1514,10 @@ void DebuggerPluginPrivate::updatePresetState()
     } else if (state == DebuggerFinished) {
         // We don't want to do anything anymore.
         m_startAction.setEnabled(canRun);
+        if (!BuildManager::isBuilding()) {
+            m_startAction.setVisible(!SessionManager::startupProject()
+                    || SessionManager::startupProject()->needsConfiguration() || canRun);
+        }
         m_debugWithoutDeployAction.setEnabled(canRun);
         m_visibleStartAction.setAction(ActionManager::command(DEBUGGER_START)->action());
         m_hiddenStopAction.setAction(&m_undisturbableAction);
