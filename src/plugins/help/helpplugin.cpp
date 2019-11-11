7a543d8717b0753e3d0844684d849caefae036a6
Martin Kampas <martin.kampas@jolla.com>
2019-11-11 09:35:27 +0100

    Revert "Revert "Help: Remove QtWebKit backend""
    This reverts commit 98ff0f74d6efd7ef3cd2c9494291bfdf2948e695.

--- src/plugins/help/helpplugin.cpp
+++ src/plugins/help/helpplugin.cpp
@@ -40,7 +40,6 @@
 #include "localhelpmanager.h"
 #include "openpagesmanager.h"
 #include "openpagesmodel.h"
-#include "qtwebkithelpviewer.h"
 #include "remotehelpfilter.h"
 #include "searchwidget.h"
 #include "searchtaskhandler.h"
@@ -456,9 +455,6 @@ HelpViewer *HelpPlugin::createHelpViewer(qreal zoom)
     using ViewerFactory = std::function<HelpViewer *()>;
     using ViewerFactoryItem = QPair<QByteArray, ViewerFactory>; // id -> factory
     QVector<ViewerFactoryItem> factories;
-#ifndef QT_NO_WEBKIT
-    factories.append(qMakePair(QByteArray("qtwebkit"), []() { return new QtWebKitHelpViewer(); }));
-#endif
 #ifdef QTC_WEBENGINE_HELPVIEWER
     factories.append(qMakePair(QByteArray("qtwebengine"), []() { return new WebEngineHelpViewer(); }));
 #endif
