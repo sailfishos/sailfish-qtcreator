86f011c45fc6519af49022685e44b42f8198e8c7
Martin Kampas <martin.kampas@jolla.com>
2020-01-27 08:10:45 +0100

    Mer: Shuffle widgets on general options page

--- src/plugins/mer/mergeneraloptionswidget.cpp
+++ src/plugins/mer/mergeneraloptionswidget.cpp
@@ -30,6 +30,8 @@
 
 #include <sfdk/sdk.h>
 
+#include <utils/utilsicons.h>
+
 using namespace Utils;
 
 namespace Mer {
@@ -45,9 +47,13 @@ MerGeneralOptionsWidget::MerGeneralOptionsWidget(QWidget *parent)
     m_ui->environmentFilterTextEdit->setEnabled(!MerSettings::isEnvironmentFilterFromEnvironment());
     m_ui->environmentFilterTextEdit->setToolTip(m_ui->environmentFilterTextEdit->toolTip()
             .arg(Constants::SAILFISH_SDK_ENVIRONMENT_FILTER));
-    m_ui->environmentFilterOverriddenLabel->setVisible(MerSettings::isEnvironmentFilterFromEnvironment());
-    m_ui->environmentFilterOverriddenLabel->setText(m_ui->environmentFilterOverriddenLabel->text()
-            .arg(Constants::SAILFISH_SDK_ENVIRONMENT_FILTER));
+    m_ui->environmentFilterInfoLabel->setVisible(MerSettings::isEnvironmentFilterFromEnvironment());
+    m_ui->environmentFilterInfoLabel->setPixmap(Utils::Icons::WARNING.pixmap());
+    m_ui->environmentFilterInfoLabel->setToolTip(
+            QLatin1String("<font color=\"red\">")
+            + tr("This option is currently overridden with the %1 environment variable.")
+                .arg(Constants::SAILFISH_SDK_ENVIRONMENT_FILTER)
+            + QLatin1String("</font>"));
 
     m_ui->rpmValidationByDefaultCheckBox->setToolTip(m_ui->rpmValidationByDefaultCheckBox->toolTip()
             .arg(MerRpmValidationStep::displayName())
