d34068c71d29ae4addf86d4fc26b569d2ddd59aa
Martin Kampas <martin.kampas@jolla.com>
2020-02-24 10:59:03 +0100

    QMakeStep: Allow to run nonrecursively
    This is meant to be used programmatically by tools that rely on qmake
    execution for the top level project only.
    
    (cherry picked from commit 7d2fe724699726fb02905b2667fcb7507827a044)

--- src/plugins/qmakeprojectmanager/qmakestep.cpp
+++ src/plugins/qmakeprojectmanager/qmakestep.cpp
@@ -124,7 +124,7 @@ QString QMakeStep::allArguments(const BaseQtVersion *v, ArgumentFlags flags) con
     else
         arguments << project()->projectFilePath().toUserOutput();
 
-    if (v->qtVersion() < QtVersionNumber(5, 0, 0))
+    if (m_recursive && v->qtVersion() < QtVersionNumber(5, 0, 0))
         arguments << "-r";
     bool userProvidedMkspec = false;
     for (QtcProcess::ConstArgIterator ait(m_userArgs); ait.next(); ) {
@@ -199,7 +199,7 @@ bool QMakeStep::init()
         workingDirectory = qmakeBc->buildDirectory();
 
     m_qmakeCommand = CommandLine{qtVersion->qmakeCommand(), allArguments(qtVersion), CommandLine::Raw};
-    m_runMakeQmake = (qtVersion->qtVersion() >= QtVersionNumber(5, 0 ,0));
+    m_runMakeQmake = m_recursive && (qtVersion->qtVersion() >= QtVersionNumber(5, 0 ,0));
 
     QString makefile = workingDirectory.toString() + '/';
 
@@ -293,6 +293,12 @@ void QMakeStep::doRun()
 void QMakeStep::setForced(bool b)
 {
     m_forced = b;
+}
+
+// Used by the 3rd party SailfishOS plugin
+void QMakeStep::setRecursive(bool b)
+{
+    m_recursive = b;
 }
 
 void QMakeStep::processStartupFailed()
@@ -433,7 +439,7 @@ QString QMakeStep::effectiveQMakeCall() const
         QmakeBuildConfiguration *qmakeBc = qmakeBuildConfiguration();
         const QString makefile = qmakeBc ? qmakeBc->makefile() : QString();
         result += ' ' + allArguments(qtVersion, ArgumentFlag::Expand);
-        if (qtVersion->qtVersion() >= QtVersionNumber(5, 0, 0))
+        if (m_recursive && qtVersion->qtVersion() >= QtVersionNumber(5, 0, 0))
             result.append(QString::fromLatin1(" && %1 %2").arg(make).arg(makeArguments(makefile)));
     }
     return result;
