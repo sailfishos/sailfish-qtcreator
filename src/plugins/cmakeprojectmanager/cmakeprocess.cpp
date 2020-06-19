c327b7fb64ee4d73c132804e0841bad62002c774
Martin Kampas <martin.kampas@jolla.com>
2020-06-19 14:16:20 +0200

    CMakeProjectManager: Fix false configuration failure
    The "Configuring <project>" progress item would always end in red color.
    Compare CMakeProcess::reportFinished and CMakeProcess::reportCanceled.
    
    (cherry picked from commit b30c62338fa265b49688de8c4a817d7ee322bd50)

--- src/plugins/cmakeprojectmanager/cmakeprocess.cpp
+++ src/plugins/cmakeprojectmanager/cmakeprocess.cpp
@@ -235,6 +235,7 @@ void CMakeProcess::handleProcessFinished(int code, QProcess::ExitStatus status)
     }
 
     m_future->reportFinished();
+    m_future.reset();
 
     emit finished(code, status);
 
