55f0850306649e1862e1f3f093e2484dd2b6c207
Ville Nummela <ville.nummela@jolla.com>
2020-07-08 10:32:58 +0300

    CMakeProjectManager: Generate Unix Makefiles even on Windows
    (cherry picked from commit 659de4009e0767fe3cbf5fb294c5b71eb9500ac1)
    (cherry picked from commit a68e41ab6f8347ca089a9f07845307fe079055a1)

--- src/plugins/cmakeprojectmanager/cmakekitinformation.cpp
+++ src/plugins/cmakeprojectmanager/cmakekitinformation.cpp
@@ -681,13 +681,15 @@ QVariant CMakeGeneratorKitAspect::defaultValue(const Kit *k) const
                               known.constEnd(),
                               [](const CMakeTool::Generator &g) {
                                   return g.matches("NMake Makefiles")
-                                         || g.matches("NMake Makefiles JOM");
+                                         || g.matches("NMake Makefiles JOM")
+                                         || g.matches("Unix Makefiles");
                               });
             if (ProjectExplorerPlugin::projectExplorerSettings().useJom) {
                 it = std::find_if(known.constBegin(),
                                   known.constEnd(),
                                   [](const CMakeTool::Generator &g) {
-                                      return g.matches("NMake Makefiles JOM");
+                                      return g.matches("NMake Makefiles JOM")
+                                         || g.matches("Unix Makefiles");
                                   });
             }
 
