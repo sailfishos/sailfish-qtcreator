sailfishos/sailfish-qtcreator

commit df5a64ddb4b624dca939f71447cc644b6bebe724

Sfdk: Forward --snapshots-of argument to sdk-assistant
Co-authored-by: martyone <martin.kampas@jolla.com>

src/tools/sfdk/command.cpp
@@ -1660,12 +1660,24 @@ Worker::ExitStatus BuiltinWorker::runTools(const QStringList &arguments_, int *e
     }
 
     if (arguments.first() == "remove") {
-        if (!P::checkPositionalArgumentsCount(arguments, 2, 2))
+        QCommandLineParser parser;
+        QCommandLineOption snapshotsOfOption("snapshots-of");
+
+        parser.addOptions({snapshotsOfOption});
+        parser.addPositionalArgument("name", QString(), "[name]");
+
+        if (!parser.parse(arguments)) {
+             qerr() << parser.errorText() << endl;
+             return BadUsage;
+        }
+
+        if (!P::checkPositionalArgumentsCount(parser.positionalArguments(), 1, 1))
             return BadUsage;
 
-        const QString name = arguments.at(1);
+        const QString name = positionalArguments.first();
 
-        *exitCode = SdkManager::removeTools(name, typeHint) ? EXIT_SUCCESS : EXIT_FAILURE;
+        *exitCode = SdkManager::removeTools(name, typeHint, parser.isSet(snapshotsOfOption))
+            ? EXIT_SUCCESS : EXIT_FAILURE;
         return NormalExit;
     }
 

src/tools/sfdk/sdkmanager.cpp
@@ -549,7 +549,7 @@ public:
         return exitCode == EXIT_SUCCESS;
     }
 
-    bool removeTools(const QString &name, SdkManager::ToolsTypeHint typeHint)
+    bool removeTools(const QString &name, SdkManager::ToolsTypeHint typeHint, bool snapshotsOf)
     {
         if (!fetchInfo())
             return false;
@@ -559,10 +559,12 @@ public:
         if (!packageForName(name, typeHint, &package, &flags))
             return false;
 
-        if (flags & ToolsInfo::UserDefined || flags & ToolsInfo::Snapshot) {
+        if (flags & ToolsInfo::UserDefined || flags & ToolsInfo::Snapshot || snapshotsOf) {
             QStringList args;
             args += toArgs(typeHint);
             args += "remove";
+            if (snapshotsOf)
+                args += "--snapshots-of";
             args += name;
             const int exitCode = SdkManager::runOnEngine("sdk-assistant", args);
             return exitCode == EXIT_SUCCESS;
@@ -1170,10 +1172,10 @@ bool SdkManager::installCustomTools(const QString &name, const QString &imageFil
     return ToolsPackageManager().installCustomTools(name, imageFileOrUrl, typeHint, maybeTooling);
 }
 
-bool SdkManager::removeTools(const QString &name, ToolsTypeHint typeHint)
+bool SdkManager::removeTools(const QString &name, ToolsTypeHint typeHint, bool snapshotsOf)
 {
     QTC_ASSERT(s_instance->hasEngine(), return false);
-    return ToolsPackageManager().removeTools(name, typeHint);
+    return ToolsPackageManager().removeTools(name, typeHint, snapshotsOf);
 }
 
 BuildTargetData SdkManager::configuredTarget(QString *errorMessage)

src/tools/sfdk/sdkmanager.h
@@ -142,7 +142,7 @@ public:
     static bool installTools(const QString &name, ToolsTypeHint typeHint);
     static bool installCustomTools(const QString &name, const QString &imageFileOrUrl,
             ToolsTypeHint typeHint, const QString &maybeTooling);
-    static bool removeTools(const QString &name, ToolsTypeHint typeHint);
+    static bool removeTools(const QString &name, ToolsTypeHint typeHint, bool snapshotsOf);
 
     static BuildTargetData configuredTarget(QString *errorMessage);
 

commit e6d6aabaaaa6ebcf2a6d7f463fd8f1310a354ffb

Sfdk: Fix compile error
Broken since commit df5a64ddb4b624dca939f71447cc644b6bebe724.

src/tools/sfdk/command.cpp
@@ -1674,7 +1674,7 @@ Worker::ExitStatus BuiltinWorker::runTools(const QStringList &arguments_, int *e
         if (!P::checkPositionalArgumentsCount(parser.positionalArguments(), 1, 1))
             return BadUsage;
 
-        const QString name = positionalArguments.first();
+        const QString name = parser.positionalArguments().first();
 
         *exitCode = SdkManager::removeTools(name, typeHint, parser.isSet(snapshotsOfOption))
             ? EXIT_SUCCESS : EXIT_FAILURE;